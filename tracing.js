b8a5eeec80ecad70032b031367ded426a9187cbc
uhussain11 <uhussain@ufl.edu>
2025-12-03 13:42:08 -0500

    Observability assignment: OTEL + Jaeger implemented

---
+++ tracing.js
@@ -0,0 +1,48 @@
+// tracing.js
+
+const { resourceFromAttributes } = require('@opentelemetry/resources');
+const { ATTR_SERVICE_NAME } = require('@opentelemetry/semantic-conventions');
+const { NodeSDK } = require('@opentelemetry/sdk-node');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
+const {
+  ConsoleMetricExporter,
+  PeriodicExportingMetricReader,
+} = require('@opentelemetry/sdk-metrics');
+
+const { ExpressInstrumentation } = require('opentelemetry-instrumentation-express');
+const { MongoDBInstrumentation } = require('@opentelemetry/instrumentation-mongodb');
+const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
+
+const resource = resourceFromAttributes({
+  [ATTR_SERVICE_NAME]: 'todo-service',
+});
+
+const traceExporter = new OTLPTraceExporter({
+  url: 'http://localhost:4318/v1/traces',
+});
+
+const sdk = new NodeSDK({
+  resource,
+  traceExporter,
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new ConsoleMetricExporter(), // metrics → console
+  }),
+  instrumentations: [
+    getNodeAutoInstrumentations(),
+    new ExpressInstrumentation(),
+    new MongoDBInstrumentation(),
+    new HttpInstrumentation(),
+  ],
+});
+
+sdk.start();
+console.log('OpenTelemetry SDK started');
+
+process.on('SIGTERM', () => {
+  sdk
+    .shutdown()
+    .then(() => console.log('OpenTelemetry SDK shutdown'))
+    .catch((err) => console.error('Error shutting down OpenTelemetry SDK', err))
+    .finally(() => process.exit(0));
+});
